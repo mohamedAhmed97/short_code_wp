dd6ffad1023da2f3882da1567c4b4f6d77a11760
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2021-01-15 22:19:31 +0200

    add baseController

---
+++ Controller/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Inc\Controller;
+
+class BaseController
+{
+    protected $plugin_path;
+    protected $plugin_name;
+    protected $plugin_name_short_code;
+    public function __construct()
+    {
+        $this->plugin_name = plugin_basename(dirname(__FILE__, 3) . "/first_plugin.php");
+        $this->plugin_path = plugin_dir_path(dirname(__FILE__, 2));
+        $this->plugin_name_short_code = plugin_basename(dirname(__FILE__, 3));
+    }
+}
