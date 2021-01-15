038f008db510d8be3844de35769ae5446388822e
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2021-01-16 00:06:47 +0200

    fix style path in Assets.php

--- inc/Base/Assets.php
+++ inc/Base/Assets.php
@@ -12,9 +12,10 @@ class Assets extends BaseController
 
     public function addAsset($links)
     {
-        wp_register_style("bootstrap", $this->plugin_path . "/assets/css/bootstrap.min.css");
+        wp_register_style("bootstrap", $this->plugin_assets_path . "/assets/css/bootstrap.min.css");
         wp_enqueue_style("bootstrap");
         
-        wp_enqueue_script("bootstrap", $this->plugin_path . "/assets/js/bootstrap.min.js");
+        wp_register_script("bootstrap", $this->plugin_assets_path . "/assets/js/bootstrap.min.js");
+        wp_enqueue_script("bootstrap");
     }
 }
--- inc/Controller/BaseController.php
+++ inc/Controller/BaseController.php
@@ -5,6 +5,7 @@ namespace Inc\Controller;
 class BaseController
 {
     protected $plugin_path;
+    protected $plugin_assets_path;
     protected $plugin_name;
     protected $plugin_name_short_code;
     public function __construct()
@@ -12,5 +13,6 @@ class BaseController
         $this->plugin_name = plugin_basename(dirname(__FILE__, 3)."/object_plugin.php");
         $this->plugin_path = plugin_dir_path(dirname(__FILE__, 2));
         $this->plugin_name_short_code=plugin_basename(dirname(__FILE__,3));
+        $this->plugin_assets_path=plugins_url()."/objects_plugin";
     }
 }
