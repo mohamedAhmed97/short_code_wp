de7324a2dad63f4aa4f157810b9ca4ca76b0f11e
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2021-01-15 22:22:29 +0200

    add plugin's setting link && add Init.php

---
+++ Base/SettingLink.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Inc\Base;
+
+use \Inc\Controller\BaseController;
+
+class SettingLink extends BaseController
+{
+    public function register()
+    {
+        add_filter("plugin_action_links_$this->plugin_name", array($this, 'settingLink'));
+    }
+
+    public function settingLink($links)
+    {
+        $setting_link = '<a href="admin.php?page=objects_plugin">setting</a>';
+        array_push($links, $setting_link);
+        return $links;
+    }
+}
